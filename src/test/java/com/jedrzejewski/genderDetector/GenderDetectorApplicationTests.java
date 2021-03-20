c35533b190a9b17c117350eb4485d9ea1acd4dcf
Arkodiusz <arkadiusz.jedrzej@gmail.com>
2021-03-20 23:12:12 +0100

    initial commit

---
+++ src/test/java/com/jedrzejewski/genderDetector/GenderDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jedrzejewski.genderDetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderDetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
