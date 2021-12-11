6354b209a672d940ddc2c43916d07ce7692d4f61
Arkodiusz <arecki358@gmail.com>
2021-12-11 15:17:30 +0100

    heroku config

--- src/main/java/com/jedrzejewski/genderdetector/GenderDetectorApplication.java
+++ src/main/java/com/jedrzejewski/genderdetector/GenderDetectorApplication.java
@@ -2,18 +2,12 @@ package com.jedrzejewski.genderdetector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GenderDetectorApplication extends SpringBootServletInitializer {
+public class GenderDetectorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GenderDetectorApplication.class, args);
 	}
 
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(GenderDetectorApplication.class);
-    }
 }
