29d3a58a7e9a77d123bf04084fa017a65567f3bc
Arkodiusz <arecki358@gmail.com>
2021-12-12 02:39:01 +0100

    fill db script

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,64 @@
+// const url = 'http://localhost:8080'
+const url = 'https://bgenderator.herokuapp.com'
+
+const fillDb = async (gender) => {
+
+    let genderSign;
+
+    if (gender === 'male') {
+        genderSign = 'm'
+    } else if (gender === 'female') {
+        genderSign = 'f'
+    } else {
+        console.log('wrong gender')
+        return
+    }
+
+    const names = await fetch(`${gender}.txt`)
+        .then(response => response.text())
+        .then(text => text.split('\n'))
+
+    const chunks = divideNameListToChunks(names, 50)
+
+    for (const chunk of chunks) {
+        const tokens = []
+
+        chunk.forEach(name => {
+            let tokenDto = {
+                name: name,
+                gender: genderSign
+            }
+            tokens.push(tokenDto)
+            console.log(tokenDto)
+        })
+
+        const body = JSON.stringify(tokens)
+        console.log(body)
+
+        await fetch(`${url}/api/tokens/fill`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: body
+        })
+            .then(response => response.json())
+            .then(result => {
+                console.log('Success:', result);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+}
+
+function divideNameListToChunks(arr, chunkSize) {
+    if (chunkSize <= 0) throw "Invalid chunk size";
+    var R = [];
+    for (var i=0,len=arr.length; i<len; i+=chunkSize)
+        R.push(arr.slice(i,i+chunkSize));
+    return R;
+}
+
+
+
